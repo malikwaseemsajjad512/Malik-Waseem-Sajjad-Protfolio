cc452187ea2790ce49e3eae69223c61c5f9d1605
Malik Waseem Sajjad Ranjha <wasemsajjad51214@gmail.com>
2026-07-10 05:40:10 +0500

    Email Sent Message Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.querySelector('.contact-form').addEventListener('submit', async functio
         var res = await fetch(this.action, { method:'POST', body:new FormData(this), headers:{'Accept':'application/json'} });
         if (res.ok) {
             status.style.color = '#4ade80';
-            status.textContent = 'Message sent! I\'ll get back to you soon.';
+            status.textContent = 'Message Sent! Waseem will contact you soon.';
             this.reset();
         } else {
             status.style.color = '#f87171';
